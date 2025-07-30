ecd31af179ae5c618a3212f0d1e4d824a7b50bea
arunsubbiah <subbiaha@purdue.edu>
2025-07-30 14:13:36 -0400

    Initial commit

---
+++ backend/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, jsonify
+import requests
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+API_KEY = '5q4UpOLlenCwNHgPdq7qkuD3Ib3wWEU1jhnRORYV'  
+
+@app.route('/apod')
+def apod():
+    url = f'https://api.nasa.gov/planetary/apod?api_key={API_KEY}'
+    response = requests.get(url)
+    data = response.json()
+    return jsonify(data)
+
+@app.route('/mars')
+def mars():
+    url = f'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key={API_KEY}'
+    response = requests.get(url)
+    data = response.json()
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
